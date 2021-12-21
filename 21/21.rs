cdf3097e148539df7d51616c1c37011d7169e4fa
Rasmus Hallbäck <rasmushallback01@gmail.com>
2021-12-21 16:33:37 +0100

    Day 21

--- 21/21.rs
+++ 21/21.rs
@@ -7,7 +7,6 @@ impl DeterministicDice {
 	fn new() -> DeterministicDice {
 		return DeterministicDice {current_value: 0, total_rolls: 0}
 	}
-
 	fn roll(&mut self) -> u64 {
 		self.current_value += 1;
 		if self.current_value > 100 {
@@ -16,12 +15,12 @@ impl DeterministicDice {
 		self.total_rolls+=1;
 		return self.current_value;
 	}
-
 	fn get_total_rolls(&self) -> u64 {
 		return self.total_rolls;
 	}
 }
 
+#[derive(Clone)]
 struct Player {
 	position: u64,
 	score: u64
@@ -31,7 +30,6 @@ impl Player {
 	fn new(start_position: u64) -> Player {
 		return Player {position: start_position, score: 0};
 	}
-
 	fn step(&mut self, steps: u64) {
 		self.position += steps;
 		while self.position > 10 {
@@ -41,33 +39,32 @@ impl Player {
 	}
 }
 
-fn quantum_solve(rolls: &std::collections::HashMap<u64, usize>, who: u64, position0: u64, position1: u64, score0: u64, score1: u64) -> (usize,usize) {
-	if score0 >= 21 {
+fn quantum_solve(rolls: &std::collections::HashMap<u64, usize>, turn: bool, p1: &Player, p2: &Player) -> (usize,usize) {
+	if p1.score >= 21 {
 		return (1, 0);
-	} 
-	if score1 >= 21 {
+	}
+	if p2.score >= 21 {
 		return (0, 1);
 	}
-	let mut total0 = 0;
-	let mut total1 = 0;
-	
+	let mut total_wins_1 = 0;
+	let mut total_wins_2 = 0;
 	for (roll, amount) in rolls.iter() {
-		if who == 0 {
-			let mut position = position0 + roll; while position > 10 {position-=10;}
-			let score = score0 + position;
-			let (result0, result1) = quantum_solve(&rolls, 1, position, position1, score, score1);
-			total0 += result0 * amount;
-			total1 += result1 * amount;
+		if turn {
+			let mut temp_player = p2.clone();
+			temp_player.step(*roll);
+			let (result_wins_1, result_wins_2) = quantum_solve(&rolls, false, &p1, &temp_player);
+			total_wins_1 += result_wins_1 * amount;
+			total_wins_2 += result_wins_2 * amount;
 		} else {
-			let mut position = position1 + roll; while position > 10 {position-=10;}
-			let score = score1 + position;
-			let (result0, result1) = quantum_solve(&rolls, 0, position0, position, score0, score);
-			total0 += result0 * amount;
-			total1 += result1 * amount;
+			let mut temp_player = p1.clone();
+			temp_player.step(*roll);
+			let (result_wins_1, result_wins_2) = quantum_solve(&rolls, true, &temp_player, &p2);
+			total_wins_1 += result_wins_1 * amount;
+			total_wins_2 += result_wins_2 * amount;
 		}
 	}
-	return (total0, total1);
-} 
+	return (total_wins_1, total_wins_2);
+}
 
 fn dirac_dice_game(p1s: u64, p2s: u64) -> usize {
 	let mut rolls = std::collections::HashMap::new();
@@ -82,7 +79,7 @@ fn dirac_dice_game(p1s: u64, p2s: u64) -> usize {
 			}
 		}
 	}
-	let (r1, r2) = quantum_solve(&rolls, 0, p1s, p2s, 0, 0);
+	let (r1, r2) = quantum_solve(&rolls, false, &Player::new(p1s), &Player::new(p2s));
 	return if r1 > r2 {r1} else {r2};
 }
 
@@ -102,15 +99,7 @@ fn practise_game(p1s: u64, p2s: u64) -> u64 {
 	}
 }
 
-fn part1() {
-	println!("Part 1 Output: {}", practise_game(4, 2));	
-}
-
-fn part2() {
-	println!("Part 2 Output: {}", dirac_dice_game(4, 2));
-}
-
 fn main() {
-	part1();
-	part2();
+	println!("Part 1 Output: {}", practise_game(4, 2));
+	println!("Part 2 Output: {}", dirac_dice_game(4, 2));
 }
